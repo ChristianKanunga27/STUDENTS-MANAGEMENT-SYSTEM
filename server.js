78109a1bb5b60f6d77d6d84934e8675fb50c394f
christian <kanungachiristian27@gmail.com>
2026-04-29 00:27:22 +0300

    fix error

--- server.js
+++ server.js
@@ -428,7 +428,9 @@ async function fetchStudentAttendance(user) {
  
 // PAGES
 
-//app.get("/", (req, res) => res.send("SERVER RUNNING"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.get("/login", (req, res) =>
     res.sendFile(path.join(__dirname, "public/index.html"))
